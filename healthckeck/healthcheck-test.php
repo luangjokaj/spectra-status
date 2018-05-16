00af8a3fb8b5282de6ecc3b1844515ed52b1e3b2
Luan Gjokaj <luan@riangle.com>
2018-05-16 10:29:00 +0200

    Add healthchecks.

---
+++ healthckeck/healthcheck-test.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json');
+// create a new cURL resource
+error_reporting(E_ALL);
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, 'http://test.spectra.io/healthcheck');
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// copy the handle
+$ch2 = curl_copy_handle($ch);
+
+// grab URL (http://www.example.com/) and pass it to the browser
+$result = curl_exec($ch2);
+
+// close cURL resources, and free up system resources
+curl_close($ch2);
+curl_close($ch);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+?>
