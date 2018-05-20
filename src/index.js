4a69c4fbced9c11383dd7224970583ddff9b35a4
Luan Gjokaj <luan@riangle.com>
2018-05-20 20:16:30 +0200

    Add analytics.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-119541922-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
